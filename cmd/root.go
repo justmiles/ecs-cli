657166792914cd3c5c48e0d049cf13c7504b09ca
Miles Maddox <j.milesmaddox@gmail.com>
2021-11-11 08:59:44 -0600

    set version during build

--- cmd/root.go
+++ cmd/root.go
@@ -12,17 +12,17 @@ var (
 
 // Configure the root command
 var rootCmd = &cobra.Command{
-	Use:     "ecs",
-	Short:   "Manage ECS",
-	Long:    `A lightweight tool for working with ECS`,
-	Version: "0.0.19",
+	Use:   "ecs",
+	Short: "Manage ECS",
+	Long:  `A lightweight tool for working with ECS`,
 	Run: func(cmd *cobra.Command, args []string) {
 		cmd.Help()
 	},
 }
 
 // Execute validates input the Cobra CLI
-func Execute() {
+func Execute(version string) {
+	rootCmd.Version = version
 	if err := rootCmd.Execute(); err != nil {
 		log.Fatal(err)
 	}
--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"github.com/justmiles/ecs-cli/cmd"
 )
 
+// version of ecs-cli. Overwritten during build
+var version = "development"
+
 func main() {
-	cmd.Execute()
+	cmd.Execute(version)
 }
